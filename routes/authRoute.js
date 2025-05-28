b2337801da9ff956fc787bcadbb89f3b3d9cd4ee
Yashhh2409 <dhande.yash2001@gmail.com>
2025-05-28 19:13:57 +0530

    middleware updated

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,11 +4,13 @@ const {
   signup,
   login,
   getAuthCustomers,
+  addUser,
 } = require("../controllers/authController");
 const veryfyToken = require('../middlewares/authMiddleware.js')
 
-router.post("/signup", signup);
+router.post("/signup", signup)
 router.post("/login", login);
+router.post("/adduser", veryfyToken, addUser);
 router.get("/getCustomers", veryfyToken, getAuthCustomers);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const categoryRoutes = require('./routes/categoryRoutes');
 const recommendedRestarountsRoute = require('./routes/recommendedVendersRoute');
-const authRoutes = require('./routes/authRoute')
+const userRoute = require('./routes/user/userRoute.js')
 const logRoutes = require('./routes/customerLogsRoute')
 // const db = require('./config/DB.js')
 const locationLogRoutes = require('./routes/customerLocationLogsRoute.js')
@@ -16,13 +16,16 @@ const PORT = process.env.PORT || 8000;
 
 // Enable CORS for all origins
 app.use(cors());
-
 app.use(bodyParser.json());
+app.use(express.json());
 
 // api routes
 app.use('/api/categories', categoryRoutes);
 app.use('/api/recommendations', recommendedRestarountsRoute);
-app.use('/api/auth', authRoutes);
+
+
+// app.use('/api/auth', authRoutes);
+app.use('', userRoute);
 
 app.use('/api', logRoutes);
 
